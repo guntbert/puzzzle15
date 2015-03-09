9aae857feb19288c9537b17935610d794113f37d
G. Reiter <guntbert@gmx.at>
2015-03-09 20:26:51 +0100

    initial commit
    mit Ablaufplan

---
+++ puzzle15/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle15
+{
+    /* Ablauf
+     * 
+     * 1) Platzaufteilung, Geometrie
+     * 
+     * 2) Chips erzeugen
+     * 
+     * 3) Zufallsalgorithmus
+     * 
+     * 4) Verschiebelogik
+     * 
+     * */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
